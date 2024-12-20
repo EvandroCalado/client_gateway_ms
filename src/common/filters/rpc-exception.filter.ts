7536745fb1612294923075c7caf5c8c9936861dd
Evandro Calado <evandrocalado07@gmail.com>
2024-12-20 15:54:45 -0300

    feat: enhance RpcExceptionFilter to handle empty response errors

--- src/common/filters/rpc-exception.filter.ts
+++ src/common/filters/rpc-exception.filter.ts
@@ -1,4 +1,9 @@
-import { ArgumentsHost, Catch, ExceptionFilter } from '@nestjs/common';
+import {
+  ArgumentsHost,
+  Catch,
+  ExceptionFilter,
+  HttpStatus,
+} from '@nestjs/common';
 import { RpcException } from '@nestjs/microservices';
 
 @Catch(RpcException)
@@ -10,6 +15,16 @@ export class RpcExceptionFilter<T extends RpcException>
     const response = context.getResponse();
 
     const error = exception.getError();
+
+    if (error.toString().includes('Empty response')) {
+      return response.status(HttpStatus.INTERNAL_SERVER_ERROR).json({
+        statusCode: HttpStatus.INTERNAL_SERVER_ERROR,
+        message: error
+          .toString()
+          .substring(0, error.toString().indexOf('(') - 1),
+        error: 'Internal server error',
+      });
+    }
 
     return response.status(error['statusCode']).json(error);
   }
